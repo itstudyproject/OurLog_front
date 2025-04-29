808c16e4be48d0caf13cc4d3ca681a28d012c10b
XII1071 <garmen1071@gmail.com>
2025-04-29 19:13:55 +0900

    0429 header disign nav

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-// import { Box } from "./pages/Box/box.tsx";
-
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
 
 createRoot(document.getElementById("app") as HTMLElement).render(
   <StrictMode>
-    {/* <Box /> */}
     <App />
   </StrictMode>
 );
