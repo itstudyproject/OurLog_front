a25cc52bab1f6a06f3b8137e1d4397f6d592203d
Ethualo <lkh0926ym@naver.com>
2025-05-02 15:28:19 +0900

    gh

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from '../components/Header'
 import '../styles/Header.css'
+import Footer from '../components/Footer';
 
 export default function MainLayout() {
   const [scrollWidth, setScrollWidth] = useState(0);
