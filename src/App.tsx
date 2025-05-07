9f42491f8ef939d12d6b140cfa2b20b3a9d6c4c5
shrewd-sea <a86197779@gmail.com>
2025-05-07 20:55:42 +0900

    0507

--- src/App.tsx
+++ src/App.tsx
@@ -17,27 +17,33 @@ import DeleteAccountPage from "./pages/DeleteAccountPage";
 import ArtList from "./pages/Art/ArtList";
 import RankingPage from "./pages/Ranking/RankingPage";
 import PostDetail from "./pages/Post/PostDetail";
+import Payment from "./pages/Art/Payment";
 import PostModify from "./pages/Post/PostModify";
-// import Header from './layouts/Header';
+import ArtDetail from "./pages/Art/ArtDetail";
+import BidHistory from "./pages/Art/BidHistory";
 function App() {
   return (
     <Routes>
-      {/* 메인 레이아웃을 사용하는 페이지들 */}
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
-        <Route path="terms-condition" element={<TermsCondition />} />
         <Route path="customer-center" element={<CustomerCenter />} />
         <Route path="privacy-policy" element={<PrivacyPolicy />} />
+
         <Route path="art">
           <Route index element={<ArtList />} />
+          <Route path=":id" element={<ArtDetail />} />
+          <Route path="payment/:id" element={<Payment />} />
+          <Route path="bids" element={<BidHistory />} />
         </Route>
+
         <Route path="ranking" element={<RankingPage />} />
-      </Route>
-      <Route path="post">
-        <Route index element={<PostList />} />
-        <Route path=":id" element={<PostDetail />} />
-        <Route path="register" element={<PostRegister />} />
-        <Route path="postModify/:id" element={<PostModify />} />
+
+        <Route path="post">
+          <Route index element={<PostList />} />
+          <Route path=":id" element={<PostDetail />} />
+          <Route path="Register" element={<PostRegister />} />
+          <Route path="postModify/:id" element={<PostModify />} />
+        </Route>
       </Route>
 
       {/* 별도 레이아웃을 사용하는 페이지들 */}
